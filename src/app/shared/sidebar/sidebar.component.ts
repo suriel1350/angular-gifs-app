22fa243605721748f0355bda7bbfffa0fa3b771a
suriel_asael <kato_1350@hotmail.com>
2021-06-14 20:37:51 -0500

    project-angular_gifs_app add: add service to control history

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,11 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get historial() {
+    return this.gifsService.historial;
   }
+
+  constructor( private gifsService: GifsService ) { }
 
 }
